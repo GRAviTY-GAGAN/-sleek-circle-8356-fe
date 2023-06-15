c6289472223a722ae708e8f486cd7c9d10346948
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-15 14:40:58 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
->>>>>>> 5dc89c3d86af3c32ddc331fffef62f6a4e819da1
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
